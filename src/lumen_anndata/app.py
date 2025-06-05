a84c92988985ba8460343fb0d97adabe18f2e492
Andrew Huang <ahuang@anaconda.com>
2025-06-04 18:07:25 -0700

    Add more analyses

--- src/lumen_anndata/app.py
+++ src/lumen_anndata/app.py
@@ -2,7 +2,9 @@
 import holoviews as hv
 import lumen.ai as lmai
 
-from lumen_anndata.analysis import ManifoldMapAnalysis
+from lumen_anndata.analysis import (
+    ComputeEmbeddingAnalysis, LeidenUMAPAnalysis, ManifoldMapAnalysis,
+)
 from lumen_anndata.utils import upload_h5ad
 
 hv.extension("bokeh")
@@ -13,7 +15,7 @@ ui = lmai.ExplorerUI(
     table_upload_callbacks={
         ".h5ad": upload_h5ad,
     },
-    analyses=[ManifoldMapAnalysis],
+    analyses=[ManifoldMapAnalysis, LeidenUMAPAnalysis, ComputeEmbeddingAnalysis],
     log_level="DEBUG",
 )
 ui.servable()
--- src/lumen_anndata/views.py
+++ src/lumen_anndata/views.py
@@ -1,17 +1,74 @@
+import colorcet as cc
+import datashader as ds
+import holoviews as hv
+import panel as pn
+import param
+
+from holoviews.operation.datashader import datashade, spread
 from hv_anndata import ManifoldMap
 from lumen.views import View
 
+from .operations import ComputeEmbedding, Leiden, labeller
+
 
 class ManifoldMapPanel(View):
-    def to_spec(self, context=None):
-        return super().to_spec(context=context)
 
-    @classmethod
-    def from_spec(cls, spec):
-        return super().from_spec(spec)
+    view_type = "manifold_map"
 
     def get_panel(self):
-        import holoviews as hv
+
         hv.extension("bokeh")
 
-        return ManifoldMap(adata=self.pipeline.source._adata_store)
+        return ManifoldMap(adata=self.pipeline.get(self.pipeline.table, return_type="anndata"))
+
+
+class UMAPPanel(View):
+
+    category = param.Selector(
+        default=None,
+        objects=[
+            "leiden",
+            "cell_type",
+            "tissue",
+            "development_stage",
+            "observation_joinid",
+        ],
+        doc="Category to color points by in UMAP embedding.",
+    )
+
+    operation = param.Selector(
+        default=None,
+        objects=[ComputeEmbedding, Leiden],
+        doc="Operation to apply for UMAP embedding.",
+    )
+
+    operation_kwargs = param.Dict(
+        default={},
+        doc="Keyword arguments to pass to the selected operation.",
+    )
+
+    view_type = "umap"
+
+    def get_panel(self):
+
+        hv.extension("bokeh")
+
+        adata = self.pipeline.source.get(self.pipeline.table, return_type="anndata")
+        if self.operation:
+            adata = self.operation(**self.operation_kwargs).apply(adata)
+        data = tuple(adata.obsm["X_umap"].T)
+        vdims = []
+        agg = "count"
+        color_key = "glasbey"
+        if self.category:
+            data += (adata.obs[self.category].values,)
+            vdims = [self.category]
+            agg = ds.count_cat(self.category)
+            color_key = cc.glasbey_dark[: len(adata.obs[self.category].unique())]
+        points = hv.Points(data, vdims=vdims)
+        shaded = spread(datashade(points, aggregator=agg, color_key=color_key), px=4).opts(
+            responsive=True, height=600, xaxis=None, yaxis=None, show_legend=True, show_grid=True
+        )
+        if self.category:
+            return pn.panel(shaded * labeller(points).opts(text_color="black"))
+        return pn.panel(shaded)
