b44b855161094ff6ab5c79f95b32085d97a9ad89
Andrew Huang <ahuang@anaconda.com>
2025-08-22 15:26:05 -0700

    clean up layout

--- src/lumen_anndata/controls.py
+++ src/lumen_anndata/controls.py
@@ -154,8 +154,8 @@ class CellXGeneSourceControls(SourceControls):
             self._tabulator,
         )
         original_controls = super().__panel__()
-        return pn.Tabs(
-            ("Upload Datasets", original_controls),
-            ("CELLxGENE Census Datasets", czi_controls),
-            dynamic=True,
-        )
+        # Append to input tabs; add check to prevent duplicate
+        if len(original_controls[0]) == 2:
+            original_controls[0].append(("CELLxGENE Census Datasets", czi_controls))
+            original_controls[0].active = len(original_controls[0]) - 1
+        return original_controls
